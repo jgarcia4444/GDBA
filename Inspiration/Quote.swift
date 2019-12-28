acd9383032809447695f532223484b3c8c2e2382
Jake Garcia <bballwiz4@gmail.com>
2019-12-27 16:08:58 -0800

    In the middle of creating functionality to display a random quote

---
+++ Inspiration/Quote.swift
@@ -0,0 +1,16 @@
+//
+//  Quote.swift
+//  Inspiration
+//
+//  Created by Jake Garcia on 12/27/19.
+//  Copyright © 2019 Jake Garcia. All rights reserved.
+//
+
+import Foundation
+
+struct Quote {
+    
+    let text: String?
+    let author: String?
+    
+}
--- Inspiration/ViewController.swift
+++ Inspiration/ViewController.swift
@@ -12,20 +12,34 @@ import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
     
+    var quotesArr: [Quote]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        getQuotes()
+    }
+    
+    func getQuotes() {
         let session  = URLSession.shared
         let url = URL(string: "https://type.fit/api/quotes")!
         let task = session.dataTask(with: url, completionHandler: { data, response, error in
             let dataJSON = JSON(data)
+            
             print(dataJSON)
+            
+            
         })
         task.resume()
     }
 
+    @IBAction func generateQuoteButtonPressed(_ sender: UIButton) {
+        
+        
+        
+    }
 }
 
